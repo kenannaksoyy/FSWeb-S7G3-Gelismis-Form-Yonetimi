621ceef88a426216a71db970a5fdc07328d79844
Kenan AKSOY <kenannaksoyy@gmail.com>
2022-12-07 21:18:47 +0300

    Kenan Aksoy

--- kullanici-katilimi/src/Form.js
+++ kullanici-katilimi/src/Form.js
@@ -3,19 +3,19 @@ import axios from "axios";
 import * as yup from "yup";
 
 const schema = yup.object().shape({
-    isimSoyisim: yup
+    user: yup
     .string()
     .required("İsim-Soyisim Gerekli")
     .max(45,"İsim-Soyisim Çok Uzun"),
 
     email: yup
     .string()
-    .email('Geçersiz e-mail adresi')
+    .email("Mail Fortmati Uygun Degil")
     .required("e-mail gerekli"),
 
-    sifre: yup
+    password: yup
     .string()
-    .required("İsim-Soyisim Gerekli")
+    .required("Sifre Gerekli")
     .min(8,"Şifre 8 Karakterden Uzun Olmalı")
     .max(20,"Şifre 20 Karakterden Fazla Olamaz"),
     
@@ -23,150 +23,145 @@ const schema = yup.object().shape({
 
   });
 
-function Form() {
-    const [katilimci, setKatilimci] = useState({
-        isimSoyisim:"",
-        email:"",
-        sifre:"",
-        kullanimSartlari:false
-      });
-    
-      const [hatalar, setHatalar] = useState({
-        isimSoyisim:"",
-        email:"",
-        sifre:"",
-        kullanimSartlari:false
-      })
-
+  function Form() {
+    const [form, setForm] = useState({
+      user: "",
+      email: "",
+      agree: false,
+      password: ""
+    });
+    const [errors, setErrors] = useState({
+      user: "",
+      star: "",
+      agree: "",
+      password: ""
+    });
     const [disabled, setDisabled] = useState(true);
     const [userId, setUserId] = useState("");
-
+  
     useEffect(() => {
-        schema.isValid(katilimci).then((valid) => setDisabled(!valid));
-    }, [katilimci]);
-
-    const formHataKontrol = (name, value) => {
-        yup
-          .reach(schema, name)
-          .validate(value)
-          .then(() => {
-            setHatalar({
-              ...hatalar,
-              [name]: ""
-            });
-          })
-          .catch((err) => {
-            setHatalar({
-              ...hatalar,
-              [name]: err.hatalar[0]
-            });
+      schema.isValid(form).then((valid) => setDisabled(!valid));
+    }, [form]);
+  
+    const checkFormErrors = (name, value) => {
+      yup
+        .reach(schema, name)
+        .validate(value)
+        .then(() => {
+          setErrors({
+            ...errors,
+            [name]: ""
           });
-    };
-    
-    const handleChange = (event) => {
-        const { checked, name, value, type } = event.target;
-        const valueToUse = type === "checkbox" ? checked : value;
-    
-        formHataKontrol(name, valueToUse);
-    
-        setKatilimci({
-          ...katilimci,
-          [name]: valueToUse
+        })
+        .catch((err) => {
+          setErrors({
+            ...errors,
+            [name]: err.errors[0]
+          });
         });
     };
-
+  
+    const handleChange = (event) => {
+      const { checked, name, value, type } = event.target;
+      const valueToUse = type === "checkbox" ? checked : value;
+      checkFormErrors(name, valueToUse);
+  
+      setForm({
+        ...form,
+        [name]: valueToUse
+      });
+    };
+  
+  
     const handleSubmit = (event) => {
-        event.preventDefault();
-    
-        const newUser = {
-          isimSoyisim: katilimci.isimSoyisim.trim(),
-          email: katilimci.email.trim(),
-          sifre: katilimci.sifre.trim(),
-          kullanimSartlari: katilimci.kullanimSartlari
-        };
-    
-        axios
-          .post("https://reqres.in/api/user", newUser)
-          .then((res) => {
-            setUserId(res.data.id);
-    
-            setKatilimci({
-              isimSoyisim: "",
-              email: "",
-              sifre: "",
-              kullanimSartlari: false
-            });
-          })
-          .catch((err) => {
-            debugger;
-          });
+      event.preventDefault();
+  
+      const newUser = {
+        user: form.user,
+        email: form.email,
+        agree: form.agree,
+        password: form.password
       };
-    
+  
+      axios
+        .post("https://reqres.in/api/user", newUser)
+        .then((res) => {
+          setUserId(res.data.id);
+  
+          setForm({
+            user: "",
+            star: "",
+            agree: false,
+            password: ""
+          });
+        })
+        .catch((err) => {
+          debugger;
+        });
+    };
+  
+    return (
+      <div className="App">
+        <div style={{ color: "red" }}>
+          <div>{errors.user}</div>
+          <div>{errors.star}</div>
+          <div>{errors.password}</div>
+          <div>{errors.agree}</div>
+        </div>
+        <form onSubmit={handleSubmit}>
 
-      return (
-        <div className="App">
-          <div style={{ color: "red" }}>
-            <div>{hatalar.isimSoyisim}</div>
-            <div>{hatalar.email}</div>
-            <div>{hatalar.sifre}</div>
-            <div>{hatalar.kullanimSartlari}</div>
-          </div>
+          <p>
+            <label htmlFor="user">İsim-Soyisim: </label>
+            <input
+              type="text"
+              id="user"
+              name="user"
+              value={form.user}
+              placeholder="Enter your name"
+              onChange={handleChange}
+            />
+          </p>
 
-          <form onSubmit={handleSubmit}>
-            <p>
-              <label htmlFor="isimSoyisim">İsim Soyisim: </label>
-              <input
-                type="text"
-                id="isimSoyisim"
-                name="isimSoyisim"
-                value={katilimci.isimSoyisim}
-                placeholder="İsim-Soyisim Giriniz"
-                onChange={handleChange}
-              />
-            </p>
-
-            <p>
-              <label htmlFor="email">e-Mail: </label>
-              <input
-                type="text"
-                id="email"
-                name="email"
-                value={katilimci.email}
-                placeholder="email Giriniz"
-                onChange={handleChange}
-              />
-            </p>
-
-            <p>
-              <label htmlFor="sifre">Sifre: </label>
+          <p>
+              <label htmlFor="password">Sifre: </label>
               <input
                 type="password"
-                id="sifre"
-                name="sifre"
-                value={katilimci.sifre}
+                id="password"
+                name="password"
+                value={form.sifre}
                 placeholder="Sifre Giriniz"
                 onChange={handleChange}
               />
-            </p>
+          </p>
 
-            <p>
-                <label htmlFor="kullanimSartlari">Kullanım şartları kabul ediyormusunuz </label>
-                <input
-                type="checkbox"
-                id="kullanimSartlari"
-                name="kullanimSartlari"
-                checked={katilimci.kullanimSartlari}
+          <p>
+              <label htmlFor="email">e-Mail: </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={form.email}
+                placeholder="email Giriniz"
                 onChange={handleChange}
+              />
+          </p>
+
+          <p>
+            <label htmlFor="giveaway">Kullanım Sartları Kabul: </label>
+            <input
+              type="checkbox"
+              id="giveaway"
+              name="agree"
+              checked={form.agree}
+              onChange={handleChange}
             />
-            </p>
-            <p>
-              <input type="submit" disabled={disabled} value="Kayit Ol" />
-            </p>
-            <p>{userId !== "" ? <h3>Katılımcı Olusturuldu: ID {userId}</h3> : null}</p>
-          </form>
-        </div>
-      );
-    }
-    
+          </p>
+          <p>
+            <input type="submit" disabled={disabled} />
+          </p>
+        </form>
+      </div>
+    );
+  }
 
 export default Form;
